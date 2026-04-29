06aed2bcffc7967329b1dd4c0373d48f6c50d67f
Zart Zort <abdullah.durusoy31@gmail.com>
2026-04-29 23:10:33 +0300

    Apartment height

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,26 @@
-use core::time;
-use rand::rng;
-use rand_distr::{Distribution, Normal};
-use std::{fmt::DebugList, thread};
-
-const ACCEL_COEF: f64 = 0.1;
-const SPEED_COEF: f64 = 0.4;
-const POS_COEF: f64 = 0.5;
-
-const DELTA_TIME: f64 = 5.;
+fn calculate_kalman_gain(meas_var: f64, esti_var: f64) -> f64 {
+    esti_var / (meas_var + esti_var)
+}
 
 fn main() {
-    let mut rng = rng();
-    let noise = Normal::new(0., 100.).unwrap();
     // Real values
-    let mut real_position = 30_250.;
-    let mut real_speed = 50.;
-    let mut real_accel = 0.;
-    // Next Predicted Values;
-    let mut next_pre_position = 30_250.;
-    let mut next_pre_speed = 50.;
-    let mut next_pre_accel = 0.;
+    let measurements: [f64; 10] = [
+        49.03, 48.44, 55.21, 49.98, 50.6, 52.61, 45.87, 42.64, 48.26, 55.84,
+    ];
+    let mut next_pre_height = 60.;
+    let mut next_pre_variance = 225.;
     // Current Predicted Values;
-    let mut curr_pre_position = 0.;
-    let mut curr_pre_speed = 0.;
-    let mut curr_pre_accel = 0.;
-    let sleep_time = time::Duration::from_millis(5000);
-    for i in 0..11 {
-        if i == 5 {
-            real_accel = 8.;
-        }
-
-        let meas = real_position + noise.sample(&mut rng);
-        // Current value predictions
-        curr_pre_position = next_pre_position + POS_COEF * (meas - next_pre_position);
-        curr_pre_speed = next_pre_speed + SPEED_COEF * ((meas - next_pre_position) / DELTA_TIME);
-        curr_pre_accel =
-            next_pre_accel + ACCEL_COEF * ((meas - next_pre_position) / (DELTA_TIME * DELTA_TIME));
-        // Predict next values
-        next_pre_position = curr_pre_position
-            + (DELTA_TIME * curr_pre_speed)
-            + (DELTA_TIME * DELTA_TIME * curr_pre_accel * 0.5);
-        next_pre_speed = curr_pre_speed + (DELTA_TIME * curr_pre_accel);
-        next_pre_accel = curr_pre_accel;
+    let mut curr_pre_height = 0.;
+    let mut curr_pre_variance = 0.;
+    for meas in measurements {
+        let kalman_gain = calculate_kalman_gain(25., next_pre_variance);
+        curr_pre_height = next_pre_height + kalman_gain * (meas - next_pre_height);
+        curr_pre_variance = (1. - kalman_gain) * next_pre_variance;
+        next_pre_height = curr_pre_height;
+        next_pre_variance = curr_pre_variance;
         println!(
-            "({}) - Real Pos: {}, Predicted Pos: {}, Diff: {}",
-            i as f64 * DELTA_TIME,
-            real_position,
-            curr_pre_position,
-            real_position - curr_pre_position
+            "Real h:{}, Predicted h: {}, Meas. h: {}",
+            50., curr_pre_height, meas
         );
-        println!(
-            "({}) - Real Speed: {}, Predicted Speed: {}, Diff: {}",
-            i as f64 * DELTA_TIME,
-            real_speed,
-            curr_pre_speed,
-            real_speed - curr_pre_speed
-        );
-        println!(
-            "({}) - Real Accel: {}, Predicted Accel: {}, Diff: {}",
-            i as f64 * DELTA_TIME,
-            real_accel,
-            curr_pre_accel,
-            real_accel - curr_pre_accel
-        );
-        // Update Real Values
-        real_position += real_speed * DELTA_TIME + real_accel * DELTA_TIME * DELTA_TIME;
-        real_speed += real_accel * DELTA_TIME;
-        thread::sleep(sleep_time);
     }
 }
